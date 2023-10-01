d265cab1c4f292a522364128f5bcf64dfb48d60a
Ghsjulian <ghsjulian@gmail.com>
2023-10-01 07:59:13 +0600

    second commit

--- js/script.js
+++ js/script.js
@@ -1,16 +1,21 @@
+"use strict";
+function ghs__(tag) {
+    return document.querySelector(tag);
+}
+
 let heading = document.querySelector(".heading");
 let count = 0;
 let countTwo = heading.dataset.title.length;
 setInterval(() => {
     var colors = [
-    "rgb(8,255,252)",
-    "rgb(255,255,255)",
-    "rgb(0,90,232)",
-    "rgb(2,255,100)",
-    "rgb(255,151,10)",
-    "rgb(255,93,10)"
-];
-var randomIndex = Math.floor(Math.random() * colors.length);
+        "rgb(8,255,252)",
+        "rgb(255,255,255)",
+        "rgb(0,90,232)",
+        "rgb(2,255,100)",
+        "rgb(255,151,10)",
+        "rgb(255,93,10)",
+    ];
+    var randomIndex = Math.floor(Math.random() * colors.length);
     heading.style.color = colors[randomIndex];
 }, 1000);
 setTimeout(myFunc, 1000);
@@ -42,3 +47,15 @@ function myFunc() {
         }
     }
 }
+
+ghs__(".mobile-nav").onclick = () => {
+    var div = ghs__(".mobile-menu"),
+        isopen = div.getAttribute("isopen");
+    if (isopen === "false") {
+        div.setAttribute("isopen","true")
+        div.style.display = "block";
+    } else {
+        div.setAttribute("isopen","false")
+        div.style.display = "none";
+    }
+};
